a31910b2a5f7ccb74540e71943f5facd767e71dd
Joseph Cunningham <joechamm@outlook.com>
2020-10-21 21:39:30 -0400

    add comments

--- app/src/main/java/com/guineacoder/diceroller/MainActivity.kt
+++ app/src/main/java/com/guineacoder/diceroller/MainActivity.kt
@@ -6,25 +6,32 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result on screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result
+     */
     private fun rollDice() {
+        // Create a new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
+
 
 class Dice(private val numSides: Int) {
     fun roll(): Int {
