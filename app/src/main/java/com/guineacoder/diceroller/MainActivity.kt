a3b435ffb0f223fd29e844f8d5108f92b50e0ba8
Joseph Cunningham <joechamm@outlook.com>
2020-10-21 21:12:27 -0400

    add dice class

--- app/src/main/java/com/guineacoder/diceroller/MainActivity.kt
+++ app/src/main/java/com/guineacoder/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.guineacoder.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,21 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
+
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
